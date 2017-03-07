bfddc2aeff7887c3811128dd1de1156ac8a1e609
EelaiWind <tony333ts@gmail.com>
2017-03-07 14:17:49 +0800

    add data and template

---
+++ cifar10/tester.py
@@ -0,0 +1,14 @@
+import sys
+# import anything else you need
+
+if len(sys.argv) < 3:
+    print 'usage: python solver.py <trained_weight> <image_list>'
+# you can add other arguments after <trained_weight> <image_list>
+
+# load image
+
+# load trained model
+
+# generate model output 
+
+
